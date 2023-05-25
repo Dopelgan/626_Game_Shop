7a273b6d6ae77b3733089fbe0e2f943f1e9d6650
Dopelgan <dopelgan21@gmail.com>
2023-05-25 17:23:34 +0300

    Создал модели Basket, Favorite и миграции к ним

---
+++ database/migrations/2023_05_25_141159_create_baskets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBasketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baskets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('game_id');
+            $table->unsignedInteger('quantity');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('game_id')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('baskets');
+    }
+}
